785cf6f7bd798fd6bed4b0ec4baf8337d62213da
Spencer Kerans <skerans0@gmail.com>
2022-06-03 19:04:33 -0600

    initial files with auth

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+
+
+
+const Home = () => {
+
+
+  return (
+    <main>
+    
+    </main>
+  );
+};
+
+export default Home;
