c5befa4e5f1fd83fd3b3c649f32cbb124f97c97e
Nick Martin <martin.nicholas.r@gmail.com>
2022-06-12 14:51:26 -0600

    repsonsive chart display and finishing touches

--- client/src/components/ViewData/index.js
+++ client/src/components/ViewData/index.js
@@ -6,10 +6,11 @@ import { Chart as ChartJS,
          CategoryScale,
          LinearScale,
          BarElement,
+         ArcElement,
          Title,
          Tooltip,
          Legend } from 'chart.js';
-import { Bar } from 'react-chartjs-2'; 
+import { Bar, Doughnut } from 'react-chartjs-2'; 
 
 import LlamaGraphicSmall from '../../images/llama-graphic-sm.svg'
 import { QUERY_SINGLE_SURVEY } from '../../utils/queries';
@@ -18,6 +19,7 @@ ChartJS.register(
     CategoryScale,
          LinearScale,
          BarElement,
+         ArcElement,
          Title,
          Tooltip,
          Legend
@@ -34,6 +36,8 @@ const ViewData = (props) => {
     let answerLabels = [];
     let answerCounts = [];
 
+    console.log(survey.upvotes);
+
     if (survey.answers && survey.answers.length > 0) {
         for (let i = 0; i < survey.answers.length; i++) {
             answerLabels.push(survey.answers[i].answerText);
@@ -42,7 +46,114 @@ const ViewData = (props) => {
     } else {
         answers = <p>There are no answers!</p>
     }
- 
+
+
+
+    // Dougnut Chart for Popularity - Static data - no useState/useEffect
+    const popularityData = {
+        labels: [
+          'Likes',
+          'Dislikes',
+        ],
+        datasets: [{
+          label: 'Popularity',
+          data: [survey.upvotes, survey.downvotes],
+          backgroundColor: [
+            '#A896FB',
+            '#FFBE76',
+          ],
+          hoverOffset: 4,
+          barThickness: .1
+        }]
+      };
+
+    const popularityOptions = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: "top"
+            },
+            title: {
+                display: true,
+                text: "Query Popularity",
+                font: {
+                    size: 28,
+                    family: "'Mukta', sans-serif",
+                    weight: 400
+                },
+                align: 'start',
+                color: '#333333',
+                padding: {
+                    top: 10,
+                    bottom: 20
+                }
+            }
+        }
+     }
+
+
+    // Mobile rendering of response data - Doughnut preferrable to bar at this size
+    // Data is static (no useState, no useEffect)
+
+     // Managing display of doughnut versus bar chart with useState and useEffect
+
+     const [viewportIsMobile, setMobileViewport] = useState(false);
+
+     useEffect(() => {
+        function handleResize() {
+          setMobileViewport(window.innerWidth < 640);
+        }
+    
+        window.addEventListener("resize", handleResize);
+    
+        handleResize();
+    
+        return () => window.removeEventListener("resize", handleResize);
+      }, []);
+
+
+
+    const answerDataMobile = {
+            labels: answerLabels,
+            datasets: [{
+              label: 'Query Responses',
+              data: answerCounts,
+              backgroundColor: ["hsla(4, 95%, 63%, .8)", "hsla(334, 63%, 55%, .8)", "hsla(317, 48%, 50%, .8)", "hsla(275, 54%, 53%, .8)", "hsla(252, 87%, 61%, .8)"],
+              borderColor: ["hsl(4, 95%, 63%)", "hsl(334, 63%, 55%)", "hsl(317, 48%, 50%)", "hsl(275, 54%, 53%)", "hsl(252, 87%, 61%)"],
+              hoverOffset: 4,
+            }]
+          };
+    
+
+    const answerOptionsMobile = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: "top"
+            },
+            title: {
+                display: true,
+                text: "Query Responses",
+                font: {
+                    size: 28,
+                    family: "'Mukta', sans-serif",
+                    weight: 400
+                },
+                align: 'start',
+                color: '#333333',
+                padding: {
+                    top: 10,
+                    bottom: 20
+                }
+            }
+        }
+    }
+
+
+    // Desktop bar chart for Survey Response data
+    // This is using useState and useEffect but is a little wonky
     const [chartData, setChartData] = useState({
         datasets: []
     });
@@ -62,7 +173,6 @@ const ViewData = (props) => {
                     borderRadius: 5,
                     categoryPercentage: .8,
                     barPercentage: .5,
-
                 }
             ]
        });
@@ -74,8 +184,8 @@ const ViewData = (props) => {
                    position: "bottom"
                },
                title: {
-                   display: false,
-                   text: survey.question,
+                   display: true,
+                   text: "Query Responses",
                    font: {
                        size: 28,
                        family: "'Mukta', sans-serif",
@@ -103,12 +213,23 @@ const ViewData = (props) => {
 
             <div className='chart-container'>
             <h2 className='interior'>{survey.question}</h2>
-            <Bar
+            <div className='doughnut-chart-container'>
+            <Doughnut data={popularityData} options={popularityOptions} />
+            </div>
+            
+            {viewportIsMobile ? (
+                <div className='doughnut-chart-container'>
+                 <Doughnut data={answerDataMobile} options={answerOptionsMobile} />
+                 </div>
+            ) : (
+                <div className="bar-chart-container">
+                <Bar
                 options={chartOptions}
                 data={chartData}
                 />
+                 </div>
+            )}
             </div>
-                
             </div>
             <img id='data-viz-llama' className='llama-watermark' src={LlamaGraphicSmall} alt="watermark of Llama" />
             </div>
